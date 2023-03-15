d7182bbd4ab09b044f89839d0f56f6a39ab36e59
Yesenia Bautista <bautiyes@yahoo.com>
2023-03-15 08:22:43 -0400

    colored hearts

--- src/components/HeartsList.js
+++ src/components/HeartsList.js
@@ -1,4 +1,5 @@
-import Heart from './Heart';
+//import Heart from './Heart';
+import CandyColoredHeart from "./CandyColoredHeart";
 
 const messages = [
     'cool cud',
@@ -27,7 +28,7 @@ const HeartsList= () => {
     return(
         <div className='hearts-container'>
             {messages.map((message,index) =>(
-                <Heart key={index} msg={message}/>
+                <CandyColoredHeart key={index} msg={message}/>
             ))}
         </div>
     );
